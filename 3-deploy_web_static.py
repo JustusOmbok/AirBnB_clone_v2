93349549257318078ec0f4e761cd4499fa967d66
JustusOmbok <ombokjustus@gmail.com>
2023-10-05 21:02:23 +0300

    pep-8 style

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """
-Fabric script (based on the file 2-do_deploy_web_static.py) 
-that creates and distributes an archive to your web servers, 
+Fabric script (based on the file 2-do_deploy_web_static.py)
+that creates and distributes an archive to your web servers,
 using the function deploy
 """
-from fabric.api import run, local, env
+from fabric.api import run, local, env, put
 from datetime import datetime
 from os.path import exists
 
 env.hosts = ["18.209.223.53", "54.160.76.99"]
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/school'
+
 
 def do_pack():
     """ Generates a .tgz archive """
@@ -26,6 +27,7 @@ def do_pack():
     else:
         return None
 
+
 def do_deploy(archive_path):
 
     """ An archive is distributed to web_servers and deployed. """
@@ -35,9 +37,12 @@ def do_deploy(archive_path):
     try:
         put(archive_path, '/tmp/')
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        release_folder = "/data/web_static/releases/web_static_{}/".format(timestamp)
+        release_folder = (
+                "/data/web_static/releases/web_static_{}/".format(timestamp)
+        )
         run("mkdir -p {}".format(release_folder))
-        run("tar -xzf /tmp/{} -C {}".format(archive_path.split("/")[-1], release_folder))
+        run("tar -xzf /tmp/{} -C {}"
+            .format(archive_path.split("/")[-1], release_folder))
         run("rm /tmp/{}".format(archive_path.split("/")[-1]))
         run("mv {}web_static/* {}".format(release_folder, release_folder))
         run("rm -rf {}web_static".format(release_folder))
@@ -48,7 +53,9 @@ def do_deploy(archive_path):
         print("New version deployed!")
         return True
     except Exception as e:
+        print("Deployment failed: {}".format(e))
         return False
+
 
 def deploy():
     """
