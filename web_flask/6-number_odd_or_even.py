47c67b2e6096939d2b3d7c6528581e2314f6d156
JustusOmbok <ombokjustus@gmail.com>
2023-10-21 13:14:55 +0300

    /hbnb routes

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -59,7 +59,9 @@ def number_odd_or_even(n):
         odd_or_even = "even"
     else:
         odd_or_even = "odd"
-    return render_template('6-number_odd_or_even.html', number=n, odd_or_even=odd_or_even)
+    return render_template('6-number_odd_or_even.html', 
+            number=n, 
+            odd_or_even=odd_or_even)
 
 
 if __name__ == '__main__':
