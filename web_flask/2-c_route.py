479a480858ff70277f9040087c0e6e1c00f0989c
JustusOmbok <ombokjustus@gmail.com>
2023-10-21 11:55:50 +0300

    /hbnb route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Flask web app with routes.
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """ Displays "Hello HBNB" at the root.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ Displays "HBNB" at /hbnb route.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_route(text):
+    """ Displays "C " followed by the value of the text.
+    """
+    return "C " + text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
