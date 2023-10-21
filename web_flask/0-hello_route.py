925429b237e12662785bb41fdd46a561371977ab
JustusOmbok <ombokjustus@gmail.com>
2023-10-21 11:06:22 +0300

    readme

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 """Instance for flask app."""
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_hbnb():
     """ Hello HBNB is printed """
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
