c4c8fd93469f5e0609bbf623de3306eb553b0bf7
JustusOmbok <ombokjustus@gmail.com>
2023-10-25 12:25:10 +0300

    filters

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Simple flask web app for displaying filters."""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/hbnb', methods=['GET'])
+def hbnb():
+    """Displays Airbnb filters."""
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    cities = sorted(storage.all(City).values(), key=lambda city: city.name)
+    amenities = sorted(storage.all(Amenity).values(),
+                       key=lambda amenity: amenity.name)
+    places = sorted(storage.all(Place).values(), key=lambda place: place.name)
+    return render_template(
+            '100-hbnb.html',
+            states=states,
+            cities=cities,
+            amenities=amenities,
+            places=places
+            )
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the db after each request."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
